28459c2675913851174787d425148a6508fc9aaa
Jowon <jowon2819@gmail.com>
2015-04-21 16:31:40 +0900

    initial version

---
+++ src/userprog/syscall.c
@@ -0,0 +1,23 @@
+#include "userprog/syscall.h"
+#include <stdio.h>
+#include <syscall-nr.h>
+#include "threads/interrupt.h"
+#include "threads/thread.h"
+
+static void syscall_handler (struct intr_frame *);
+
+// sublime Text2 test 
+// gitHub test 1
+
+void
+syscall_init (void) 
+{
+  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+}
+r
+static void
+syscall_handler (struct intr_frame *f UNUSED) 
+{
+  printf ("system call!\n");
+  thread_exit ();
+}
